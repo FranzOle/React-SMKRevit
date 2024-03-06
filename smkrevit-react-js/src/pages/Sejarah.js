79a22c9e13d76743c71d498e2cc285cb12578eea
FranzOle <115760743+FranzOle@users.noreply.github.com>
2024-03-07 00:55:36 +0700

    Video 3
    Menambahkan Gambar

--- smkrevit-react-js/src/pages/Sejarah.js
+++ smkrevit-react-js/src/pages/Sejarah.js
@@ -3,6 +3,7 @@ function Sejarah() {
       <div className="App">
         <h1>Sejarah SMK Revit</h1>
         <p>Isi Sejarah</p>
+        <img src="gambar/starwars.jpg" alt="starwarsjedifallenorder" />
       </div>
     );
   }
