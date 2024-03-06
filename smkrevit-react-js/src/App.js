ee52b7523d6fbeae1f54670f76f96fd8c75a8045
FranzOle <115760743+FranzOle@users.noreply.github.com>
2024-03-07 00:11:17 +0700

    Video 1
    Instalasi React JS

---
+++ smkrevit-react-js/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Belajar React</h1>
+      <p>Belajar React SMK Revit</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ smkrevit-react-js/src/pages/Kontak.js
@@ -0,0 +1,11 @@
+function Kontak() {
+    return (
+      <div className="App">
+        <h1>Kontak SMK Revit</h1>
+        <p>Belajar React Bersama Lionel</p>
+      </div>
+    );
+  }
+  
+  export default Kontak;
+  
