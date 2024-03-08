81dfa615bbd7bf472e13427ec3aa236d29930ff8
FranzOle <115760743+FranzOle@users.noreply.github.com>
2024-03-09 00:55:47 +0700

    Video 5
    Menambahkan CSS

--- smkrevit-react-js/src/pages/Home.js
+++ smkrevit-react-js/src/pages/Home.js
@@ -1,8 +1,14 @@
+const css = {
+    color:"Red",
+    background:"red",
+    fontSize:20
+}
+
 function Home() {
     return (
       <div className="App">
         <h1>Home SMK Revit</h1>
-        
+        <p style={css}>Selamat Datang di SMK Revitalisasi</p>
       </div>
     );
   }
--- smkrevit-react-js/src/pages/Kontak.js
+++ smkrevit-react-js/src/pages/Kontak.js
@@ -1,11 +1,16 @@
 function Kontak() {
-    return (
-      <div className="App">
-        <h1>Kontak SMK Revit</h1>
-        <p>Belajar React Bersama Lionel</p>
-      </div>
-    );
-  }
-  
-  export default Kontak;
+  return (
+    <div className="App">
+      <h1 className="judul">Kontak SMK Revit</h1>
+      <p style={{
+        fontFamily: "Times New Roman",
+        color: "red",
+        fontSize:50
+      }}>Belajar React Bersama Lionel</p>
+    </div>
+  );
+}
+
+export default Kontak;
+
   
