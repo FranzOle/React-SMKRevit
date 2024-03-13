08d2138ff912cf43add779596758f1821cd05f2d
FranzOle <115760743+FranzOle@users.noreply.github.com>
2024-03-13 19:47:39 +0700

    Video 8
    State

--- smkrevit-react-js/src/pages/Tentang.js
+++ smkrevit-react-js/src/pages/Tentang.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
+
 function Tentang() {
+  const [count, setCount] = useState(0);
+  function Tambah() {
+      setCount(count+1)
+  }
+
+  function Kurang() {
+      if (count > 0) {
+        setCount(count-1)
+      }
+  }
     return (
       <div className="App">
         <h1>Tentang Kami</h1>
         <p>Kami adalah channel pembelajaran Pemrograman</p>
-        <button type="button" className="btn btn-primary" >Button</button>
-        <button type="button" className="btn btn-success" >Button</button>
+        <button type="button" onClick={Tambah} className="btn btn-primary" >Tambah</button>
+        <button type="button" onClick={Kurang} className="btn btn-success" >Kurang</button>
+        <p>Counter : <span>{count}</span></p>
      </div>
     );
   }
