63d9566ec54f513a4257cc4a51f582f57f14ba1f
FranzOle <115760743+FranzOle@users.noreply.github.com>
2024-03-17 20:52:22 +0700

    Video 20
    Install Snippet

--- project-react-resto/react-resto/src/App.js
+++ project-react-resto/react-resto/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './Front/Nav.js';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sinau React</h1>
+      <h2>Install Snippet</h2>
+
+      <Nav />
     </div>
   );
 }
